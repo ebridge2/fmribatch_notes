51743cc390c35a79abceb1b752ecc650eddd1394
Eric Bridgeford <ebridge2@jhu.edu>
2017-12-09 16:26:23 -0500

    updated case1

--- investigations/subgraphing/case1.R
+++ investigations/subgraphing/case1.R
@@ -96,12 +96,10 @@ parse_class <- function(basepath, dsets, subjects) {
   return(list(sex=sclass, age=ageclass))#, disease=diseaseclass))
 }
 
-model.alternative <- function(samp, Y, Z, global.ssg, tstat=tstat, sig=.01, verb=FALSE, classify=FALSE) {
+model.alternative <- function(samp, Y, Z, global.ssg, nedge, tstat=tstat, verb=FALSE, classify=FALSE) {
   zset = unique(Z)
-  global.sz <- sum(global.ssg > 0)
   # alternate consists of the tstat computed between the subgraphs estimated per-z
   # with the global sub-graph
-  zset.sz <- array(NaN, dim=c(length(zset)))
   if (classify) {
     lhat <- array(NaN, dim=c(length(zset)))
   }
@@ -113,7 +111,7 @@ model.alternative <- function(samp, Y, Z, global.ssg, tstat=tstat, sig=.01, verb
     sg_ests <- sg.bern.subgraph_estimator(subset.gr, subset.Y)
     # compute test statistics per edge from the contingency matrix
     tstats <- sg.bern.edge_test(sg_ests$cont_matrix)
-    sg_edge <- sg.bern.subgraph_edge_estimation(tstats, global.sz)
+    sg_edge <- sg.bern.subgraph_edge_estimation(tstats, nedge)
     sg <- array(0, dim=c(nroi, nroi))
     sg[sg_edge] <- 1
     alt.ssgs[,,i] <- sg
@@ -126,17 +124,17 @@ model.alternative <- function(samp, Y, Z, global.ssg, tstat=tstat, sig=.01, verb
   tstat.alt <- do.call(tstat, list(alt.ssgs, global.ssg))
   ret <- list(tstat.alt=tstat.alt, alt.ssgs=alt.ssgs)
   if (classify) {
-    ret$lhat <- lhat
+    ret$lhat.alt <- lhat
   }
   return(ret)
 }
 
-model.null.permutation <- function(samp, Y, Z, global.ssg, global.est, nrep=100, tstat=tstat, sig=.01, verb=FALSE) {
+model.null.permutation <- function(samp, Y, Z, global.ssg, global.est, ne, nrep=100, tstat=tstat.jaccard, verb=FALSE) {
   nroi = dim(samp)[2]
   zset = unique(Z)
   tstat.null <- array(NaN, dim=c(nrep))
   null.counts <- array(0, dim=c(length(zset) + 1))
-
+  lhat <- array(0, dim=c(nrep, length(zset)))
   for (i in 1:nrep) {
     null.ssgs <- array(0, dim=c(nroi, nroi, length(zset)))
     yclass <- list()
@@ -161,60 +159,198 @@ model.null.permutation <- function(samp, Y, Z, global.ssg, global.est, nrep=100,
       sg_ests <- sg.bern.subgraph_estimator(dset.null.dat, dset.null.labs)
       # compute test statistics per edge from the contingency matrix
       tstats <- sg.bern.edge_test(sg_ests$cont_matrix)
-      sg_edge <- sg.bern.subgraph_edge_estimation(tstats, global.sz)
+      sg_edge <- sg.bern.subgraph_edge_estimation(tstats, ne)
       sg <- array(0, dim=c(nroi, nroi))
       sg[sg_edge] <- 1
       null.ssgs[,,j] = sg
-    }
-  }
-}
-
-subgraph.dset_lhat <- function(samp, Y, Z, global_sg, alt.counts, sg_ests, tstat=tstat) {
-  zset = unique(Z)
-  un_cts <- sort(unique(c(unique(as.numeric(alt.counts)), 0)))
-  niter <- (length(un_cts))*length(zset)
-  count = array(0, dim=c(niter))
-  dataset = array(NaN, dim=c(niter))
-  lhat = array(0, dim=c(niter))
-  for (i in un_cts) {
-    if (i > 0) {
-      sg <- which(alt.counts == i)  # get the appropriate edges
-    } else if (i == 0) {
-      sg <- which(global_sg > 0)
-    }
-
-    for (j in 1:length(zset)) {
-      ss = Z == zset[j]
-      subset.gr = samp[,,ss]
-      subset.Y = Y[ss]
-      iter <- (i)*(length(zset) + 1) + j
-      Yhat <- sg.bern.subgraph_classifier(subset.gr, sg, sg_ests$p, sg_ests$pi, sg_ests$classes)
-      lhat[iter] <- (sum(ss) - sum(Yhat$Yhat == Y[ss]))/sum(ss)
-      dataset[iter] <- zset[j]
-      count[iter] <- i
-      if (i > 0) {
-        count[iter] <- i
-      } else if (i == 0) {
-        count[iter] <- 'global sg'
+      if (classify) {
+        Yhat <- sg.bern.subgraph_classifier(subset.gr, sg_edge, sg_ests$p, sg_ests$pi, sg_ests$classes)
+        lhat[i, j] <- (sum(ss) - sum(Yhat$Yhat == subset.Y))/sum(ss)
       }
     }
-    iter <- iter + 1
-    dataset[iter] <- "global"
-    if (i > 0) {
-      count[iter] <- i
-    } else if (i == 0) {
-      count[iter] <- 'global sg'
-    }
-    Yhat <- sg.bern.subgraph_classifier(samp, sg, sg_ests$p, sg_ests$pi, sg_ests$classes)
-    lhat[iter] <- (length(Y) - sum(Yhat$Yhat == Y))/length(Y)
+    tstat.null[i] <- do.call(tstat, list(null.ssgs, global.ssg))
   }
-  result <- data.frame(count=count, dataset=dataset, lhat=lhat)
+  return(list(lhat.null=lhat, tstat.null=tstat.null))
+}
+
+model.case1 <- function(samp, Y, Z, sessions, nedges=xscale.log10(1, 3), tstat=tstat.jaccard) {
+  nroi <- dim(samp)[2]
+  zset = unique(Z)
+  result <- list()
+  for (j in 1:length(nedges)) {
+    ne <- nedges[j]
+    global.est <- sg.bern.subgraph_estimator(samp, Y)
+    # compute test statistics per edge from the contingency matrix
+    global.tstat <- sg.bern.edge_test(global.est$cont_matrix)
+    global.edge <- sg.bern.subgraph_edge_estimation(tstats, ne)
+    globa.ssg <- array(0, dim=c(nroi, nroi))
+    global.ssg[global.edge] <- 1
+    edge_result <- list()
+    for (i in 1:length(zset)) {
+      ss <- Z == zset[i]
+      ses_unique <- sample(unique(sessions[ss]))  # find the unique labels and resort
+      ss1 <- ss & sessions %in% ses_unique[1:(length(ses_unique)/2)]  # first half of sessions randomly
+      ss2 <- ss & sessions %in% ses_unique[(length(ses_unique)/2):length(ses_unique)]  # second half of sessions randomly
+      res <- model.analyze(samp[,,ss1], samp[,,ss2], Y[ss1], Y[ss2], sessions[ss1], sessions[ss2], ne,
+                           global.ssg, global.est)
+      res$dataset <- zset[i]
+      edge_result[[i]] <- res
+    }
+    edge_result$nedge <- ne
+    result[[j]] <- edge_result
+  }
+  return(result)
+}
+
+model.case2 <- function(samp, Y, Z, subjects, nedges=xscale.log10(1, 3), tstat=tstat.jaccard) {
+  nroi <- dim(samp)[2]
+  zset = unique(Z)
+  result <- list()
+  for (j in 1:length(nedges)) {
+    ne <- nedges[j]
+    global.est <- sg.bern.subgraph_estimator(samp, Y)
+    # compute test statistics per edge from the contingency matrix
+    global.tstat <- sg.bern.edge_test(global.est$cont_matrix)
+    global.edge <- sg.bern.subgraph_edge_estimation(tstats, ne)
+    globa.ssg <- array(0, dim=c(nroi, nroi))
+    global.ssg[global.edge] <- 1
+    edge_result <- list()
+    for (i in 1:length(zset)) {
+      ss <- Z == zset[i]
+      sub_unique <- sample(unique(subjects[ss]))  # find the unique labels and resort
+      ss1 <- ss & sessions %in% sub_unique[1:(length(sub_unique)/2)]  # first half of sessions randomly
+      ss2 <- ss & sessions %in% sub_unique[(length(sub_unique)/2):length(sub_unique)]  # second half of sessions randomly
+      res <- model.analyze(samp[,,ss1], samp[,,ss2], Y[ss1], Y[ss2], subjects[ss1], subjects[ss2], ne,
+                           global.ssg, global.est)
+      res$dataset <- zset[i]
+      edge_result[[i]] <- res
+    }
+    edge_result$nedge <- ne
+    result[[j]] <- edge_result
+  }
+  return(result)
+}
+
+model.case45 <- function(samp, Y, Z, nedges=xscale.log10(1, 3), tstat=tstat.jaccard) {
+  nroi <- dim(samp)[2]
+  zset = unique(Z)
+  result <- list()
+  for (j in 1:length(nedges)) {
+    ne <- nedges[j]
+    global.est <- sg.bern.subgraph_estimator(samp, Y)
+    # compute test statistics per edge from the contingency matrix
+    global.tstat <- sg.bern.edge_test(global.est$cont_matrix)
+    global.edge <- sg.bern.subgraph_edge_estimation(tstats, ne)
+    globa.ssg <- array(0, dim=c(nroi, nroi))
+    global.ssg[global.edge] <- 1
+    edge_result <- list()
+    for (i in 1:length(zset)) {
+        ss1 <- Z == zset[i]
+        sub_unique <- sample(unique(subjects[ss]))  # find the unique labels and resort
+        ss1 <- ss & sessions %in% sub_unique[1:(length(sub_unique)/2)]  # first half of sessions randomly
+        ss2 <- ss & sessions %in% sub_unique[(length(sub_unique)/2):length(sub_unique)]  # second half of sessions randomly
+        res <- model.analyze(samp[,,ss1], samp[,,ss2], Y[ss1], Y[ss2], subjects[ss1], subjects[ss2], ne,
+                             global.ssg, global.est)
+        res$dataset <- zset[i]
+        edge_result[[i]] <- res
+    }
+    edge_result$nedge <- ne
+    result[[j]] <- edge_result
+  }
+  return(result)
+}
+
+model.case5 <- function(samp, Y, Z, nedges=xscale.log10(1, 3), tstat=tstat.jaccard) {
+  nroi <- dim(samp)[2]
+  zset = unique(Z)
+  result <- list()
+  for (j in 1:length(nedges)) {
+    ne <- nedges[j]
+    global.est <- sg.bern.subgraph_estimator(samp, Y)
+    # compute test statistics per edge from the contingency matrix
+    global.tstat <- sg.bern.edge_test(global.est$cont_matrix)
+    global.edge <- sg.bern.subgraph_edge_estimation(tstats, ne)
+    globa.ssg <- array(0, dim=c(nroi, nroi))
+    global.ssg[global.edge] <- 1
+    edge_result <- list()
+    for (i in 1:length(zset)) {
+      ss1 <- Z == zset[i]
+      for (k in (i+1):length(zset)) {
+
+      }
+      res <- model.analyze(samp[,,ss1], samp[,,ss2], Y[ss1], Y[ss2], Z[ss1], Z[ss2], ne,
+                           global.ssg, global.est)
+      res$dataset <- zset[i]
+      edge_result[[i]] <- res
+    }
+    edge_result$nedge <- ne
+    result[[j]] <- edge_result
+  }
   return(result)
 }
 
 
+model.analyze <- function(samp1, samp2, Y1, Y2, Z1, Z2, nedge, global.ssg, global.est, tstat=tstat.jaccard) {
+  samp <- abind(samp1, samp2, along=3)
+  Y <- c(Y1, Y2)
+  Z <- c(Z1, Z2)
+  # classifier built on first subset and applied to second
+  sg_ests <- sg.bern.subgraph_estimator(samp1, Y1)
+  # compute test statistics per edge from the contingency matrix
+  tstats <- sg.bern.edge_test(sg_ests$cont_matrix)
+  class1 <- sg.bern.subgraph_edge_estimation(tstats, nedge)
+
+  Yhat2 <- sg.bern.subgraph_classifier(samp2, sg_edge, sg_ests$p, sg_ests$pi, sg_ests$classes)$Yhat
+  lhat2 <- (sum(ss) - sum(Yhat == Y2))/sum(ss)
+
+  # classifier built on second subset and applied to first
+  sg_ests <- sg.bern.subgraph_estimator(samp2, Y2)
+  # compute test statistics per edge from the contingency matrix
+  tstats <- sg.bern.edge_test(sg_ests$cont_matrix)
+  class2 <- sg.bern.subgraph_edge_estimation(tstats, nedge)
+
+  Yhat1 <- sg.bern.subgraph_classifier(samp1, sg_edge, sg_ests$p, sg_ests$pi, sg_ests$classes)$Yhat
+  lhat1 <- (sum(ss) - sum(Yhat == Y2))/sum(ss)
+
+  # estimate null and alternative model
+  alt <- model.alternative(samp, Y, Z, global.ssg, nedge, tstat=tstat, classify=TRUE)
+  null <- model.null.permutation(samp, Y, Z, global.ssg, global.est, nrep=100, tstat=tstat.jaccard)
+
+  return(list(tstat.alt=alt$tstat.alt, tstat.null=null$tstat.null, lhat.alt=alt$lhat.alt,
+              lhat.null=null$lhat.null, nedge=ne, lhat1=lhat1, lhat2=lhat2))
+}
+
 # givens
 #=========================#
-sig = 0.01  # global param for significance threshold
+basepath='/data'
 nrep = 100  # number of replicates for synthetic bootstrap to get null
+
+
+nroi = 70
+atlases = c("desikan")
+dwi.dsets = c('BNU1', 'BNU3', 'HNU1', 'SWU4')
+
+graphobj <- fmriu.io.collection.open_graphs(basepath=paste(basepath,'/dwi/edgelists/', sep=""), datasets = dwi.dsets,
+                                            atlases = atlases, gname = "graphs", fmt='edgelist', rtype='array')
+graphs = graphobj$graphs
+gcpy = graphs
+datasets = graphobj$dataset
+subjects = graphobj$subjects
+sessions = graphobj$sessions
+
+sexpath = paste(basepath,'/phenotypic/', sep="")
+class = parse_class(sexpath, dwi.dsets, subjects)
+sexs = class$sex
+# dwi.diseases = class$disease
+ages = class$age
+
+dwi.graphs = graphs[!is.nan(ages) & !is.nan(sexs),,]
+dwi.datasets = datasets[!is.nan(ages) & !is.nan(sexs)]
+dwi.subjects = subjects[!is.nan(ages) & !is.nan(sexs)]
+dwi.sessions = sessions[!is.nan(ages) & !is.nan(sexs)]
+dwi.ages = ages[!is.nan(ages) & !is.nan(sexs)]
+dwi.sexs = sexs[!is.nan(ages) & !is.nan(sexs)]
+
+dwi.bin_graphs = apply(dwi.graphs, c(2,3), function(x) thresh_matrix(x, thresh=0))
+dwi.bin_graphs <- aperm(dwi.bin_graphs, c(2,3,1))
 
